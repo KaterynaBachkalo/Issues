5d4199c989c1df1d2f3d8fa8978bc2ff594c8671
KaterynaBachkalo <kozlovets88@gmail.com>
2024-07-09 14:55:51 +0300

    create

---
+++ src/components/ListItemAssigneeIssues.tsx
@@ -0,0 +1,48 @@
+import { Flex, Link, ListItem, Text } from "@chakra-ui/react";
+import { css } from "@emotion/react";
+import React, { FC } from "react";
+import { IIssuesProps } from "../types";
+
+const stroke = css`
+  display: inline;
+  font-size: 16px;
+  border: 1px solid rgba(17, 16, 28, 0.3);
+  margin: 0 10px;
+  height: 16px;
+`;
+
+const ListItemAssigneeIssues: FC<IIssuesProps> = ({ data }) => {
+  return (
+    <ListItem
+      bgColor="white"
+      borderRadius={20}
+      border="1px solid black"
+      listStyleType="none"
+      p={4}
+    >
+      <Text fontWeight="700" textAlign="start" color="gray.800">
+        {data.title}
+      </Text>
+      <Text textAlign="start">
+        #{data.number} opened{" "}
+        {Math.floor(
+          (new Date().getTime() - new Date(data.created_at).getTime()) /
+            (3600 * 24 * 1000)
+        )}{" "}
+        days ago
+      </Text>
+      <Flex alignItems="center" flexWrap="wrap">
+        <Link
+          href={data.user.html_url}
+          target="blank"
+          _hover={{ textDecoration: "none" }}
+        >
+          {data.user.login}
+        </Link>
+        <Text css={stroke}></Text>
+        <Text>Comments: {data.comments}</Text>
+      </Flex>
+    </ListItem>
+  );
+};
+export default ListItemAssigneeIssues;
